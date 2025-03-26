8aed2f011f24d673e0f1fd944575d42ec45e1c31
pworre <paulew@stud.ntnu.no>
2025-03-26 15:27:44 +0100

    endre pinNR

--- 1_gpio/main.c
+++ 1_gpio/main.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
 
 #define GPIO ((NRF_GPIO_REGS*)0x50000000)
-#define SW0_bt 0
-#define SW1_bt 1
-#define LED1_bt 4
-#define LED4_bt 7
+#define SW0_bt 13
+#define SW1_bt 14
+#define LED1_bt 17
+#define LED4_bt 20
 
 
 typedef struct {
