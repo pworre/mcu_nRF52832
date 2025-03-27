8c39f9ca59fd36ca7d773f7b65c36c1baccb60b5
pworre <paulew@stud.ntnu.no>
2025-03-27 10:21:02 +0100

    ny struct

--- 2_uart/uart.c
+++ 2_uart/uart.c
@@ -2,7 +2,7 @@
 #include "gpio.h"
 
 #define UART ((NRF_UART_REG*)0x40002000)
-
+/*
 typedef struct {
     volatile uint32_t TASKS_STARTRX;
     volatile uint32_t TASKS_STOPRX;
@@ -22,6 +22,52 @@ typedef struct {
     volatile uint32_t RXD;
     volatile uint32_t RESERVED3[3];
     volatile uint32_t TXD;    
+} NRF_UART_REG;
+*/
+
+typedef struct {
+	//Tasks
+	volatile uint32_t TASKS_STARTRX;
+	volatile uint32_t TASKS_STOPRX;
+	volatile uint32_t TASKS_STARTTX;
+	volatile uint32_t TASKS_STOPTX;
+	volatile uint32_t RESERVED0[3];
+	volatile uint32_t TASKS_SUSPEND;
+	volatile uint32_t RESERVED1[56];
+ 
+	//Events
+	volatile uint32_t EVENTS_CTS;
+	volatile uint32_t EVENTS_NCTS;
+	volatile uint32_t EVENTS_RXDRDY;
+	volatile uint32_t RESERVED2[4];
+	volatile uint32_t EVENTS_TXDRDY;
+	volatile uint32_t RESERVED3;
+	volatile uint32_t EVENTS_ERROR;
+	volatile uint32_t RESERVED4[7];
+	volatile uint32_t EVENTS_RXTO;
+	volatile uint32_t RESERVED5[46];
+ 
+	//Registers
+	volatile uint32_t SHORTS;
+	volatile uint32_t RESERVED6[64];
+	volatile uint32_t INTENSET;
+	volatile uint32_t INTENCLR;
+	volatile uint32_t RESERVED7[93];
+	volatile uint32_t ERRORSRC;
+	volatile uint32_t RESERVED8[31];
+	volatile uint32_t ENABLE;
+	volatile uint32_t RESERVED9;
+	volatile uint32_t PSELRTS;
+	volatile uint32_t PSELTXD;
+	volatile uint32_t PSELCTS;
+	volatile uint32_t PSELRXD;
+	volatile uint32_t RXD;
+	volatile uint32_t TXD;
+	volatile uint32_t RESERVED10;
+	volatile uint32_t BAUDRATE;
+	volatile uint32_t RESERVED11[17];
+	volatile uint32_t CONFIG;
+ 
 } NRF_UART_REG;
 
 #define RTS_bt 5
