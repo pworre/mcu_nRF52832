4bc14634c109a61c38f9ec2786ae4545afb7acbd
pworre <paulew@stud.ntnu.no>
2025-03-27 10:40:45 +0100

    aiaiaia

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -38,13 +38,13 @@ int main(){
 		}else if (buttonTwoPressed) {
             uart_send('B');
 		}
-        /*
+        
         if (uart_read() != '\0'){
             for(int i = LED1_bt; i <= LED4_bt; i++){
                 GPIO->OUT ^= (1 << i);
 	        }
         }
-        */
+        
     }
 
     return 0;
