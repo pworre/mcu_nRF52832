0caf38304a1010f6d4e8cc4f7ba82749f71bedab
pworre <paulew@stud.ntnu.no>
2025-04-08 10:27:54 +0200

    test

--- 3_gpiote/ppi.c
+++ 3_gpiote/ppi.c
@@ -2,12 +2,12 @@
 #include "gpiote.h"
 
 void ppi_init(){
-    PPI->PPI_CH[0].EEP = (uint32_t)&(GPIOTE->EVENTS_IN[0]);
     for (int channel = 0; channel < 4; channel++){
+        PPI->PPI_CH[channel].EEP = (uint32_t)&(GPIOTE->EVENTS_IN[0]);
         PPI->PPI_CH[channel].TEP = (uint32_t)&(GPIOTE->TASKS_OUT[1 + channel]);
         PPI->CHENSET |= (1 << channel);
     }
     
-    PPI->PPI_TASKS[0].EN = 1;
+    //PPI->PPI_TASKS[0].EN = 1;
 
 }
