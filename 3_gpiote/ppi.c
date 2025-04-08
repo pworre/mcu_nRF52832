aa6b1a59fc12f751d891f951404b86e6ddec50cf
pworre <paulew@stud.ntnu.no>
2025-04-08 10:27:24 +0200

    merge

--- 3_gpiote/ppi.c
+++ 3_gpiote/ppi.c
@@ -3,7 +3,11 @@
 
 void ppi_init(){
     PPI->PPI_CH[0].EEP = (uint32_t)&(GPIOTE->EVENTS_IN[0]);
-    PPI->PPI_CH[0].TEP = (uint32_t)&(GPIOTE->TASKS_OUT[4]);
+    for (int channel = 0; channel < 4; channel++){
+        PPI->PPI_CH[channel].TEP = (uint32_t)&(GPIOTE->TASKS_OUT[1 + channel]);
+        PPI->CHENSET |= (1 << channel);
+    }
+    
     PPI->PPI_TASKS[0].EN = 1;
-    PPI->CHENSET = (1 << 0);
+
 }
