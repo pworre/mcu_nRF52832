b62609fc03624df132492ff2b0579d23a678f946
pworre <paulew@stud.ntnu.no>
2025-04-08 10:02:11 +0200

    CHENSET

--- 3_gpiote/ppi.c
+++ 3_gpiote/ppi.c
@@ -5,5 +5,5 @@ void ppi_init(){
     PPI->PPI_CH[0].EEP = (uint32_t)&(GPIOTE->EVENTS_IN[0]);
     PPI->PPI_CH[0].TEP = (uint32_t)&(GPIOTE->TASKS_OUT[4]);
     PPI->PPI_TASKS[0].EN = 1;
-    PPI->CHEN = (1 << 0);
+    PPI->CHENSET = (1 << 0);
 }
