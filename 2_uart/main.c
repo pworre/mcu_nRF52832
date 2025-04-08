2bbdbac7493a044a3e5b8f6f48b73b776bb3b723
hanstomr <hanstomr@stud.ntnu.no>
2025-04-08 09:06:33 +0200

    IO-funksjon

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -1,12 +1,22 @@
 #include <stdint.h>
 #include "uart.h"
 #include "gpio.h"
+#include <stdio.h>
+#include <sys/types.h> // For ssize_t
 
 #define SW0_bt 13
 #define SW1_bt 14
 #define LED1_bt 17
 #define LED4_bt 20
 
+ssize_t _write(int fd, const void *buf, size_t count){
+    char * letter = (char *)(buf);
+    for(int i = 0; i < count; i++){
+    uart_send(*letter);
+    letter++;
+    }
+    return count;
+    }
 
 
 void button_init(){ 
@@ -31,6 +41,7 @@ int main(){
 		GPIO->OUT |= (1 << i);
 	}
     //int sleep = 0;
+    iprintf("The average grade in TTK%d was in %d was: %c\n\r",4235,2022,'B');
     while(1){
         int buttonOnePressed = !(GPIO->IN & (1 << SW0_bt));
 		int buttonTwoPressed = !(GPIO->IN & (1 << SW1_bt));
