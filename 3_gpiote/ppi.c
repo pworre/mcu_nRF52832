583be039f83316a5c7bb06021b32c1acc545fc60
pworre <paulew@stud.ntnu.no>
2025-04-08 09:58:11 +0200

    switchet rekkefølge

--- 3_gpiote/ppi.c
+++ 3_gpiote/ppi.c
@@ -2,8 +2,8 @@
 #include "gpiote.h"
 
 void ppi_init(){
-    PPI->PPI_CH[0].EEP = (uint32_t)&(GPIOTE->EVENTS_IN[4]);
-    PPI->PPI_CH[0].TEP = (uint32_t)&(GPIOTE->TASKS_OUT[0]);
+    PPI->PPI_CH[0].EEP = (uint32_t)&(GPIOTE->EVENTS_IN[0]);
+    PPI->PPI_CH[0].TEP = (uint32_t)&(GPIOTE->TASKS_OUT[4]);
     PPI->PPI_TASKS[0].EN = 1;
     PPI->CHEN = (1 << 0);
 }
