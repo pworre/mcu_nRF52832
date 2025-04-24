0af050b0ef8d198137ba51f7fc53ef80650549b5
pworre <paulew@stud.ntnu.no>
2025-04-24 16:58:40 +0200

    la til rising edge

--- 3_gpiote/gpiote.c
+++ 3_gpiote/gpiote.c
@@ -7,7 +7,7 @@
 #define GPIO_EVENT_LED
 
 void GPIOTE_init(){
-    GPIOTE->CONFIG[GPIO_EVENT_SW1] = (1 << 0) | (SW0_bt << 8) | (3 << 16);    // SW1 som EVENT       OBS!!! SJekk om må endre initial value i registeret!
+    GPIOTE->CONFIG[GPIO_EVENT_SW1] = (1 << 0) | (SW0_bt << 8) | (2 << 16);    // SW1 som EVENT       OBS!!! SJekk om må endre initial value i registeret!
     int task_number = 1;
     for (int i = LED1_bt; i <= LED4_bt; i++){
         GPIOTE->CONFIG[task_number] = (3 << 0) | (i << 8) | (3 << 16); // LED as EVENT and Toggle polarity
